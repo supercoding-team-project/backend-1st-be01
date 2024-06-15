bf7aff0c6edb3bdcb016ea2ad2892fea2f414947
usnimoj <jominsu0103@naver.com>
2024-06-15 18:47:46 +0900

    Feat : 좋아요 등록 구현

--- src/main/java/com/github/backend_1st/service/likeService/LikeService.java
+++ src/main/java/com/github/backend_1st/service/likeService/LikeService.java
@@ -1,7 +1,11 @@
 package com.github.backend_1st.service.likeService;
 
+import com.github.backend_1st.repository.comments.CommentEntity;
+import com.github.backend_1st.repository.comments.CommentJpaRepository;
 import com.github.backend_1st.repository.likes.LikeEntity;
 import com.github.backend_1st.repository.likes.LikeJpaRepository;
+import com.github.backend_1st.repository.users.UserEntity;
+import com.github.backend_1st.repository.users.UserJpaRepository;
 import com.github.backend_1st.service.mapper.likes.LikeMapper;
 import com.github.backend_1st.web.dto.likes.Likes;
 import lombok.RequiredArgsConstructor;
@@ -14,10 +18,26 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class LikeService {
     private final LikeJpaRepository likeJpaRepository;
+    private final CommentJpaRepository commentJpaRepository;
+    private final UserJpaRepository userJpaRepository;
 
     public List<Likes> findLikeByCommentId(String commentId) {
         Integer commentIdInt = Integer.valueOf(commentId);
         List<LikeEntity> likeEntities = likeJpaRepository.findByCommentId(commentIdInt);
         return likeEntities.stream().map(LikeMapper.INSTANCE::likeEntityToLikes).collect(Collectors.toList());
+    }
+
+    public String saveLike(String commentId, Integer userId) {
+        Integer commentIdInt = Integer.valueOf(commentId);
+        CommentEntity comment = commentJpaRepository.findById(commentIdInt)
+                .orElseThrow(() -> new RuntimeException("Comment can not found"));
+        UserEntity user = userJpaRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User can not found"));
+        LikeEntity likeEntity = LikeEntity.builder()
+                .comment(comment)
+                .user(user)
+                .build();
+        likeJpaRepository.save(likeEntity);
+        return "like post success";
     }
 }
--- src/main/java/com/github/backend_1st/web/controller/likes/LikeController.java
+++ src/main/java/com/github/backend_1st/web/controller/likes/LikeController.java
@@ -1,25 +1,48 @@
 package com.github.backend_1st.web.controller.likes;
 
+import com.github.backend_1st.config.security.JwtTokenProvider;
 import com.github.backend_1st.service.likeService.LikeService;
 import com.github.backend_1st.web.dto.likes.LikeResponse;
 import com.github.backend_1st.web.dto.likes.Likes;
+import com.github.backend_1st.web.util.JwtUtil;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class LikeController {
     private final LikeService likeService;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Value("${spring.jwt.secret}")
+    private String secretKey;
     @GetMapping("/likes/{commentId}")
     public LikeResponse findLikeByCommentId(@PathVariable String commentId){
         List<Likes> likes = likeService.findLikeByCommentId(commentId);
         return new LikeResponse(likes);
+    }
+    @PostMapping("/likes/{commentId}")
+    public ResponseEntity<Map<String , String>> addLike(@PathVariable String commentId , HttpServletRequest request){
+        String token = jwtTokenProvider.resolveToken(request);
+        if (token != null && jwtTokenProvider.validateToken(token)) {
+            Integer userId = jwtTokenProvider.getUserIdFromToken(token); // 사용자 ID 추출
+            String returnMessage = likeService.saveLike(commentId, userId);
+            Map<String, String> response = new HashMap<>();
+            response.put("message", returnMessage);
+            return ResponseEntity.ok(response);
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of("error", "Invalid or expired token"));
+        }
     }
 
 }
