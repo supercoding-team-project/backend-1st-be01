98caafd0a4be32d816977afa7d67187b779d5dec
usnimoj <jominsu0103@naver.com>
2024-06-15 15:24:50 +0900

    Feat : likeController - 특정 댓글의 좋아요 목록 조회 구현

---
+++ src/main/java/com/github/backend_1st/web/controller/likes/LikeController.java
@@ -0,0 +1,20 @@
+package com.github.backend_1st.web.controller.likes;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class LikeController {
+    private final LikeService likeService;
+    @GetMapping("/likes/{commentId}")
+    public LikesResponse findLikeByCommentId(@PathVariable String commentId){
+        List<Likes> likes = likeService.findLikeByCommentId();
+        return new LikeResponse(likes);
+    }
+
+}
